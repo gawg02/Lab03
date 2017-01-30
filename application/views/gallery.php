c9e37ae0e5e9e2f63a05fa8a637342d2dcc2d899
gicadin <hristea52@gmail.com>
2017-01-29 16:58:08 -0800

    merged part2 and part3

--- application/views/gallery.php
+++ application/views/gallery.php
@@ -1,3 +1,8 @@
+
+{thetable}
+<h1> The image gallery, complete :) </h1>
+
+<!-- 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
@@ -25,7 +30,12 @@
                 <table cols="3" class="gallery">
                     <tr>
                         <td class="oneimage">
+
                             <a href="./data/Katmai_Crater_1980.jpg" data-lightbox="gallery" data-title="Katmai Crater - Mount Katmai, Alaska ... Posted 2014.05.05 by donald, in landscape"><img src="./data/thumb/Katmai_Crater_1980.jpg"/></a>
+                            <a href='./data/{filename}' data-lightbox='gallery'
+                                data-title='{title} ... Posted {uploaded} by {uploader}, in {category}'>
+                                <img src='./data/thumb/{filename}' />
+                            </a>
                         </td>
                         <td class="oneimage">
                             <a href="./data/Caledonian_orogeny_fold_in_King_Oscar_Fjord.jpg" data-lightbox="gallery" data-title="Caledonian orogeny fold in King Oscar Fjord ... Posted 2014.05.04 by mickey, in landscape"><img src="./data/thumb/Caledonian_orogeny_fold_in_King_Oscar_Fjord.jpg"/></a>
@@ -101,3 +111,4 @@
         <script type="text/javascript" src="js/lightbox.min.js"></script> 
     </body>
 </html>
+-->
